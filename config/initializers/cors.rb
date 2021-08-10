037452ab743421df0289f9b49f4995aee328082b
Pat <patmifsud@me.com>
2021-08-11 08:27:53 +1000

    Fixing extra comma in cors file

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ['http://localhost:3000', 'http://localhost:3001'], # Github url goes here
+    origins ['http://localhost:3000', 'http://localhost:3001'] # Github url goes here
     resource '*',
       :headers => :any,
       :methods => %i( get post put patch delete options head ) # array of symbols
